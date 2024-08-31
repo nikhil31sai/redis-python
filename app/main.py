95787bfdd2b66abf6cc1f306c40d18241d99277a
Nikhil Reddy <nikhil31sai@gmail.com>
2024-08-31 17:58:23 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -10,9 +10,12 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, address = server_socket.accept() # wait for client
 
-    req = conn.recv(1024).decode("utf-8")
-    if req == "*1\r\n$4\r\nPING\r\n":
-        conn.send("+PONG\r\n".encode("utf-8"))
+    while True:
+
+        req = conn.recv(1024).decode("utf-8")
+        print(req)
+        if req == "*1\r\n$4\r\nPING\r\n":
+            conn.send("+PONG\r\n".encode("utf-8"))
 
 
 
