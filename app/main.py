2032cd2b2def2f95e24de2dcc463c6f47c8c6567
Nikhil Reddy <nikhil31sai@gmail.com>
2024-09-04 07:00:48 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -36,6 +36,7 @@ def handle_conn(conn, address, data):
             ans = None
             if req[1] in data:
                 print(data[req[1]])
+                print(datetime.datetime.now().timestamp()*1000)
                 if(data[req[1]][1] < datetime.datetime.now().timestamp()*1000):
                     ans = data[req[1]][0].encode('utf-8')
                 else:
