2eb3a27202bc27686c7d20d761d9adbed936cb28
Nikhil Reddy <nikhil31sai@gmail.com>
2024-09-13 08:29:58 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -72,6 +72,11 @@ def handle_conn(conn, address, data):
                 if req[1] == "*":
                     ans = parser.read_rdb_key(data['dir'], data['dbfilename'])
                 resp = parser.encode(ans)
+            elif req[0] == "INFO":
+                ans = None
+                if req[1] == "REPLICATION":
+                    ans = bytes("role:master")
+                resp = parser.encode(ans)
             conn.send(resp)
             print("Sending response")
     except Exception as ex:
