2629fe964a6c88800ff1d9dacbc5519699da3913
Nikhil Reddy <nikhil31sai@gmail.com>
2024-09-13 08:31:26 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -73,7 +73,7 @@ def handle_conn(conn, address, data):
                     ans = parser.read_rdb_key(data['dir'], data['dbfilename'])
                 resp = parser.encode(ans)
             elif req[0] == "INFO":
-                ans = bytes("role:master")
+                ans = "role:master".bytes()
                 resp = parser.encode(ans)
             conn.send(resp)
             print("Sending response")
